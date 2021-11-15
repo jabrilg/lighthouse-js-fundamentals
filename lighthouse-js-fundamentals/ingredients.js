d7ad8ea527ee2acb6835b638937c3011a46d80cb
jabril gadhane <93634886+jabrilg@users.noreply.github.com>
2021-11-14 20:41:29 -0500

    Add files via upload

---
+++ lighthouse-js-fundamentals/ingredients.js
@@ -0,0 +1,6 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+console.log("list of ingredients:");
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
